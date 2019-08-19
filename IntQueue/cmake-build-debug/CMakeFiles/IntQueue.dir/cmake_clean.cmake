6c00664bf8cbae1f8cb92b2e430d1ea4278103c1
musicandelectronics <kjh970407@naver.com>
2019-08-19 14:12:16 +0900

    Hello

---
+++ IntQueue/cmake-build-debug/CMakeFiles/IntQueue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IntQueue.dir/main.c.o"
+  "IntQueue.pdb"
+  "IntQueue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IntQueue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
